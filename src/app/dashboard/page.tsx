656f98187215fedef27ce18bf3afeabde44720fe
Himanshu Gupta <himanshucse49@gmail.com>
2025-12-08 18:55:17 +0530

    Sync user

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashboardPage = () => {
+  return (
+    <div className='flex items-center justify-center min-h-screen'>
+      dashboard
+    </div>
+  )
+}
+
+export default DashboardPage
