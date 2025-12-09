d5365a717cd898e0890fce66e86adcadaf32af4b
Himanshu Gupta <himanshucse49@gmail.com>
2025-12-09 20:32:33 +0530

    Implemented Doctors Management

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,10 @@
+import Navbar from '@/components/Navbar'
 import React from 'react'
 
 const DashboardPage = () => {
   return (
     <div className='flex items-center justify-center min-h-screen'>
+      <Navbar/>
       dashboard
     </div>
   )
